92e262a93381da59fb635f3cb49e5822ee513b6b
harrypotter0 <9654263057akashkandpal@gmail.com>
2018-01-03 15:45:38 +0530

    Jinja works !!

--- pydjango/mysite/mysite/urls.py
+++ pydjango/mysite/mysite/urls.py
@@ -13,11 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url, include
 from django.contrib import admin
-from django.urls import path
-from django.conf.urls import url, include 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('webapp/', include('webapp.urls'))
+    url(r'^admin/', admin.site.urls),
+    url(r'^$', include('personal.urls')),
 ]
